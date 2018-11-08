7eab6fb0cbbb2f989098d882517d2556cd34fd17
Subir Dey <subir.raju@northsouth.edu>
2018-11-09 03:57:24 +0600

    Fixing infinite loops

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@ using namespace std;
 int main()
 {
 
-    Customer c1("01833975394","17167289279","Robi");
 
-    c1.printInformations();
+    Customer c1("01833975394","17167289279","Robi");
 
 
     c1.insertPhoneOp("01559972979","Teletalk");
-
+    c1.insertPhoneOp("01777299456","GP");
+    c1.printInformations();
     c1.printCustomerInfo();
 
-
-    cout<<"press 1 for inserting new number and operator , 2 for deleting a phone number , 3 for changing operator."<<endl;
+    while(true){
+    cout<<"\n\npress 1 for inserting new number and operator , \n\n2 for deleting a phone number , \n\n3 for changing operator \n\n 4 for Exit."<<endl;
     string number;
     string operatorrr;
     int option;
@@ -28,6 +28,7 @@ int main()
         cin>>number;
         c1.insertPhoneOp(number,operatorrr);
         cout<<"inserted successfully"<<endl;
+        c1.printCustomerInfo();
     }
     else if(option==2){
         cout<<"Enter the number to delete"<<endl;
@@ -43,8 +44,18 @@ int main()
         c1.changeOperator(number,operatorrr);
 
     }
+
+    else if(option==4)
+        {return 0;}
+
     else
-        cout<<"You have entered a wrong input"<<endl;
+       {
+
+       cout<<"You have entered a wrong input"<<endl;
+       }
+
+    }
+
 
     return 0;
 }

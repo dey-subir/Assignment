992bd7efa740d52ca85c10fae064f77eabba21bb
Subir Dey <subir.raju@northsouth.edu>
2018-11-10 14:16:48 +0600

    Modification

--- Person.h
+++ Person.h
@@ -7,16 +7,44 @@
 
 using namespace std;
 
+
 class Person{
 
 private:
+    string personName;
     string nidNumber;
+    string fatherName;
+    string motherName;
+    string birthDate;
+    string presentAddress;
+    string permanentAddress;
+    Person* arrayOfCustomer;
+    PhoneNumber* arrayOfPhone;
+
 public:
     Person();
     Person(string nid);
-    string getNidNumbers();
-    void insertNidNumbers(string nid);
+    Person(string nid,string phone,string telephoneOPerator);
+    Person(string personName,string nid,string fatherName,string motherName,string birthDate,string presentAddress,string permanentAddress);
+    Person(string personName,string nid,string fatherName,string motherName,string birthDate,string presentAddress);
+    void insertNidNumber(string nid);
+    void insertPersonName(string personName);
+    void insertFatherName(string fatherName);
+    void insertMotherName(string motherName);
+    void insertBirthDate(string birthDate);
+    void insertPresentAddress(string presentAddress);
+    void insertPermanentAddress(string permanentAddress);
+
+    string getNidNumber();
+    string getPersonName();
+    string getFatherName();
+    string getMotherName();
+    string getBirthDate();
+    string getPresentAddress();
+    string getPermanentAddress();
+
     void printInformations();
+    void printInfo();
 };
 
 
